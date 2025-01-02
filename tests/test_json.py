3798e16b15743f5abe703440289afac587ad983b
IceFox99 <huyuefeng99@gmail.com>
2025-01-02 21:41:42 +0900

    Small fixes on serialization module.

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,28 +1,31 @@
-from Bugbee.src import util
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(\
+        os.path.dirname(__file__), '../src')))
+from utils import to_json
 
 obj1 = 1
-print(util.to_json(obj1))
+print(to_json(obj1))
 
 obj2 = 1.1
-print(util.to_json(obj2))
+print(to_json(obj2))
 
 obj3 = "str"
-print(util.to_json(obj3))
+print(to_json(obj3))
 
 obj4 = b"str"
-print(util.to_json(obj4))
+print(to_json(obj4))
 
 obj5 = [1, 2, 3]
-print(util.to_json(obj5))
+print(to_json(obj5))
 
 obj6 = {1, 2, 3}
-print(util.to_json(obj6))
+print(to_json(obj6))
 
 obj7 = (1, 2, 3)
-print(util.to_json(obj7))
+print(to_json(obj7))
 
 obj8 = {"s1": 1, "s2": 2, "s3": 3}
-print(util.to_json(obj8))
+print(to_json(obj8))
 
 
 class classA:
@@ -31,7 +34,7 @@ class classA:
 
 
 obj9 = classA("fs")
-print(util.to_json(obj9))
+print(to_json(obj9))
 
 
 class classB:
@@ -47,16 +50,16 @@ obj11 = classC()
 obj10.attr = obj11
 obj11.attr = obj10
 
-print(util.to_json(obj10))
+print(to_json(obj10))
 
 obj12 = {}
 obj13 = {"obj12":obj12}
 obj12["obj13"] = obj13
-print(util.to_json(obj12))
+print(to_json(obj12))
 
 
 def func1():
     pass
 
 
-print(util.to_json(func1))
+print(to_json(func1))
